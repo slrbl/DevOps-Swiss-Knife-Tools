2a88908a85a6779f5158ddfbcba12e5d16a9d834
slrbl <walid.daboubi@gmail.com>
2016-09-30 16:09:40 +0200

    Update ChckFolderSizeNContent.sh

--- ChckFolderSizeNContent.sh
+++ ChckFolderSizeNContent.sh
@@ -1,1 +1,8 @@
-i=0;while [ TRUE ];clear;echo "$i seconds elapsed";i=$((i+1)); do du -h ./ | tail -1;echo "$(ls -1 | wc -l) items items in $(pwd)" ;sleep 1; done
+i=0
+while [ TRUE ]; do
+  clear
+  echo "$i seconds elapsed";i=$((i+1));
+  du -h ./ | tail -1
+  echo "$(ls -1 | wc -l) items in $(pwd)" 
+  sleep 1
+ done
